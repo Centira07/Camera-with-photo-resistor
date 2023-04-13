7538f45202ed1c619e680ed2d87de62da471e8ab
Centira07 <123511269+Centira07@users.noreply.github.com>
2023-04-12 23:40:21 -0400

    Update photo_Resistors.ino

--- photo_Resistors.ino
+++ photo_Resistors.ino
@@ -1,5 +1,26 @@
+int p1 = A0;
+int p2 = A1;
+int p3 = A2;
+int p4 = A3;
+int p5 = A4;
+int p6 = A5;
+
+int val1;
+int val2;
+int val3;
+int val4;
+int val5;
+int val6;
+
 void setup() {
+  pinMode(p1, INPUT);
+  pinMode(p2, INPUT);
+  pinMode(p3, INPUT);
+  pinMode(p4, INPUT);
+  pinMode(p5, INPUT);
+  pinMode(p6, INPUT);
 }
 
 void loop() {
+
 }
