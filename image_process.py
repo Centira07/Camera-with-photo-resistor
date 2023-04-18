48d836666bc693539bd4ede97398aabaad17b827
fengj10 <120220095+fengj10@users.noreply.github.com>
2023-04-17 21:24:34 -0400

    Update image_process.py

--- image_process.py
+++ image_process.py
@@ -4,20 +4,24 @@ from PIL import Image, ImageDraw
 import re
 
 # Set up serial communication with Arduino
-ser = serial.Serial()
-ser.baudrate = 9600
-ser.port = 'COM5'
+with serial.Serial('COM5', 9600, timeout=1) as ser:
+    time.sleep(2)
+    ser.readline()  # discard first line
 
-ser.open()
-time.sleep(2)
-line = ser.readline()
+    try:
+        # Read the value of the photoresistor from the serial port
+        line = ser.readline().decode().strip()
+        A0 = int(re.search(r'\d+', line).group(0))
 
-find_all = re.findall("\d+", str(line))
+        # Create a new image with a white background
+        img = Image.new('RGB', (80, 80), color=(255, 255, 255))
 
-A0 = int(find_all[0])
+        # Draw a rectangle with varying shades of grey
+        Drawer = ImageDraw.Draw(img)
+        Drawer.rectangle((0, 0, 80, 80), fill=(A0, A0, A0), outline=None)
 
-img = Image.new('RGB', (80, 80), color = (255, 255, 255))
+        # Show the image on the screen
+        img.show()
 
-Drawer = ImageDraw.Draw(img)
-Drawer.rectangle((0,0,80,80), fill=(A0,A0,A0), outline=None)
-img.show()
+    except serial.SerialException as e:
+        print(f"Serial port error: {e}")
