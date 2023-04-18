670fb33d14d26472fbe57c5db87ffe4798cf30ad
fengj10 <120220095+fengj10@users.noreply.github.com>
2023-04-17 21:20:58 -0400

    Update image_process.py
    Only single input

--- image_process.py
+++ image_process.py
@@ -1,1 +1,23 @@
+import time
+import serial
+from PIL import Image, ImageDraw
+import re
 
+# Set up serial communication with Arduino
+ser = serial.Serial()
+ser.baudrate = 9600
+ser.port = 'COM5'
+
+ser.open()
+time.sleep(2)
+line = ser.readline()
+
+find_all = re.findall("\d+", str(line))
+
+A0 = int(find_all[0])
+
+img = Image.new('RGB', (80, 80), color = (255, 255, 255))
+
+Drawer = ImageDraw.Draw(img)
+Drawer.rectangle((0,0,80,80), fill=(A0,A0,A0), outline=None)
+img.show()
